ba3dee27d03b8d233a0ae65685355fd80f39c944
Bao Do <baodo789@gmailc.om>
2026-03-21 10:53:03 +0700

    feat-setup-initial-project

---
+++ LifeTask/LifeTask/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  LifeTask
+//
+//  Created by Do Thai Bao on 21/3/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
